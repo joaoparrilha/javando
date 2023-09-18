cad69a0d1f812a60c6548ab1f4f7e5e125de6e3a
Joao Parrilha <jl.parrilha1999@gmail.com>
2023-09-18 16:43:01 -0300

    Exercicio de fixação Enum e Composição

---
+++ src/exercicioFixacaoApplication/Program.java
@@ -0,0 +1,63 @@
+package exercicioFixacaoApplication;
+
+
+
+import exercicioFixacaoEntities.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.next();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Enter order data: ");
+        System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.next());
+
+        Order order = new Order(new Date(), status, client);
+
+        System.out.print("How many items to this order? ");
+        int quantity = sc.nextInt();
+        for (int i = 1; i <= quantity; i++){
+            System.out.println("Enter #" + i + " item data:");
+            System.out.print("Product name: ");
+            sc.nextLine();
+            String productName = sc.nextLine();
+            System.out.print("Product price: ");
+            double productPrice = sc.nextDouble();
+            System.out.print("Product quantity: ");
+            int productQuantity = sc.nextInt();
+
+            Product product = new Product(productName, productPrice);
+
+            OrderItem it = new OrderItem(productQuantity, productPrice, product);
+
+            order.addItem(it);
+
+        }
+        System.out.println();
+        System.out.println(order);
+
+
+        sc.close();
+    }
+}
---
+++ src/exercicioFixacaoEntities/OrderStatus.java
@@ -0,0 +1,9 @@
+package exercicioFixacaoEntities;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
